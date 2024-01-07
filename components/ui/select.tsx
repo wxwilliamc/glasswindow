ea84f03c435eb24f154d912e8bb38c9611aee5dc
wxwilliamc <chongwx-pm18@student.tarc.edu.my>
2024-01-07 18:10:22 +0800

    add - customize Select

---
+++ components/ui/select.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@/lib/utils'
+import { ChevronDown } from 'lucide-react'
+import React, { forwardRef } from 'react'
+
+export interface SelectProps
+  extends React.HTMLProps<HTMLSelectElement> {}
+
+// eslint-disable-next-line react/display-name
+export const Select = forwardRef<HTMLSelectElement, SelectProps>(
+    ({ className, ...props }, ref) => {
+    return (
+        <div className='relative'>
+            <select className={cn('h-10 w-full border rounded-md appearance-none truncate bg-background border-input py-2 pl-3 pr-8 text-sm ring-offset-background focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', className)} ref={ref} {...props}/>
+            <ChevronDown className="absolute right-3 top-3 h-4 w-4 opacity-50" />
+        </div>
+    )
+})
+
